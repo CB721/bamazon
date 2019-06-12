cf32b283f6fb1250df13651670aa652be028eb12
CB721 <clintbrodar@gmail.com>
2019-06-12 12:51:20 -0700

    Set up key for database

---
+++ bamazonCustomer.js
@@ -0,0 +1,7 @@
+//inquirer for prompts
+var inquirer = require("inquirer");
+//mysql for database
+var mysql = require("mysql");
+//env to hide database password
+require("dotenv").config();
+
---
+++ keys.js
@@ -0,0 +1,10 @@
+//verify key file is loaded
+console.log('database is loaded');
+//export database env
+exports.database = {
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME
+};
